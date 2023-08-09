50c833b81783e9cdfd512bb6a26347d8ff2508fd
salted-hashbrown <105987464+salted-hashbrown@users.noreply.github.com>
2023-08-09 09:49:57 -0700

    review updown vote

--- src/components/ReviewCard.js
+++ src/components/ReviewCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {useState} from 'react';
+import { useState } from 'react';
 //import {FaStar} from 'react-icons/fa';
 import Rating from '@mui/material/Rating';
 import Box from '@mui/material/Box';
@@ -8,195 +8,268 @@ import Box from '@mui/material/Box';
 //---------------------------------
 //for star rating
 const labels = {
-    0.5: '1',
-    1: '2',
-    1.5: '3',
-    2: '4',
-    2.5: '5',
-    3: '6',
-    3.5: '7',
-    4: '8',
-    4.5: '9',
-    5: '10',
-  };
+  0.5: '1',
+  1: '2',
+  1.5: '3',
+  2: '4',
+  2.5: '5',
+  3: '6',
+  3.5: '7',
+  4: '8',
+  4.5: '9',
+  5: '10',
+};
 
-  const getLabelText = (value) => {
-    return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
-  }
+const getLabelText = (value) => {
+  return `${value} Star${value !== 1 ? 's' : ''}, ${labels[value]}`;
+};
 //---------------------------------
 
 
 
 const ReviewCard = (props) => {
-    //const [isExpanded, setIsExpanded] = useState(false);      //to implement "read more..." function
-    const [isUpdating, setIsUpdating] = useState(false);    //for update review
-    const [updatedReviewBody, setUpdatedReviewBody] = useState('');     //for update review
-    const [updatedRating, setUpdatedRating] = useState(props.rating / 2);       //for update review
-    const [hover, setHover] = React.useState(-1); //for star rating hoving effect
-    const [isSubmitted, setIsSubmitted] = useState(false); // State to track if the review has been submitted 
+  //const [isExpanded, setIsExpanded] = useState(false);      //to implement "read more..." function
+  const [isUpdating, setIsUpdating] = useState(false);    //for update review
+  const [updatedReviewBody, setUpdatedReviewBody] = useState('');     //for update review
+  const [updatedRating, setUpdatedRating] = useState(props.rating / 2);       //for update review
+  const [hover, setHover] = React.useState(-1); //for star rating hoving effect
+  const [isSubmitted, setIsSubmitted] = useState(false); // State to track if the review has been submitted 
+  const [isUpvoted, setIsUpvoted] = useState(props.upvotes.includes(sessionStorage.getItem('userName')));
+  const [currentUpvotes, setCurrentUpvotes] = useState(props.upvotes.length);
+  const [isDownvoted, setIsDownvoted] = useState(props.downvotes.includes(sessionStorage.getItem('userName')));
+  const [currentDownvotes, setCurrentDownvotes] = useState(props.downvotes.length);
 
-    const ReviewUpdateUrl = process.env.REACT_APP_DOMAIN + `/review/update`;
-    const ReviewDeleteUrl = process.env.REACT_APP_DOMAIN + `/review/delete`;  
 
-    let createdDate = props.createdDate.substring(0,10);  //extract the yyyy-mm-dd from the original string   
+  const ReviewUpdateUrl = process.env.REACT_APP_DOMAIN + `/review/update`;
+  const ReviewDeleteUrl = process.env.REACT_APP_DOMAIN + `/review/delete`;
 
-    //for update review---------------------------------------------------------------------------
-    const handleUpdateClick = () => {
-        setIsUpdating(true);
-        setUpdatedReviewBody(props.reviewBody);
-        setUpdatedRating(props.rating / 2);
+  let createdDate = props.createdDate.substring(0, 10);  //extract the yyyy-mm-dd from the original string   
+
+  //for update review---------------------------------------------------------------------------
+  const handleUpdateClick = () => {
+    setIsUpdating(true);
+    setUpdatedReviewBody(props.reviewBody);
+    setUpdatedRating(props.rating / 2);
+  };
+
+  const handleUpdateSubmit = () => {
+    const updateData = {
+      reviewId: props._id,
+      reviewBody: updatedReviewBody,
+      rating: updatedRating * 2,
+    };
+
+    fetch(ReviewUpdateUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(updateData),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Review updated:', data);
+        setIsUpdating(false);
+        // Handle successful update here, e.g., refresh data, show success message, etc.
+        setIsSubmitted(true); // Set the submitted state to true
+        window.location.reload();
+        setTimeout(() => { window.location.reload(); }, 500);
+      })
+      .catch((err) => {
+        console.error('An error occurred:', err);
+        // Handle error here
+      });
+  };
+
+  //--------------------------------------------------------------------------------------------
+
+
+
+  //for delete review--------------------------------------------------------------------------
+  const handleDeleteClick = () => {
+    const reviewData = {
+      reviewId: props._id
+    };
+
+    if (window.confirm('Delete this review?')) {
+      const reviewData = {
+        reviewId: props._id
       };
-    
-      const handleUpdateSubmit = () => {
-        const updateData = {
-          reviewId: props._id,
-          reviewBody: updatedReviewBody,
-          rating: updatedRating * 2,
-        };
-    
-        fetch(ReviewUpdateUrl, {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(updateData),
-        })
+
+      fetch(ReviewDeleteUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(reviewData),
+      })
         .then((res) => res.json())
         .then((data) => {
-          console.log('Review updated:', data);
-          setIsUpdating(false);
-          // Handle successful update here, e.g., refresh data, show success message, etc.
-          setIsSubmitted(true); // Set the submitted state to true
-          window.location.reload();
-          setTimeout(() => {window.location.reload();}, 500);
+          console.log('Review deleted:', data);
+          // Handle successful deletion here, e.g., redirect, update the UI, etc.
+          window.location.reload(); // Optionally, you may want to reload the page
         })
         .catch((err) => {
           console.error('An error occurred:', err);
-          // Handle error here
         });
-      };
+    };
+  };
 
-    //--------------------------------------------------------------------------------------------
+  //---------------------------------------------------------------------------------------------
 
+  // upvote downvote
 
-
-    //for delete review--------------------------------------------------------------------------
-    const handleDeleteClick = () => {
-        const reviewData = {
-          reviewId: props._id
-        };
-        
-        if (window.confirm('Delete this review?')) {
-            const reviewData = {
-              reviewId: props._id
-            };
-
-            fetch(ReviewDeleteUrl, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(reviewData),
-            })
-            .then((res) => res.json())
-            .then((data) => {
-                console.log('Review deleted:', data);
-                // Handle successful deletion here, e.g., redirect, update the UI, etc.
-                window.location.reload(); // Optionally, you may want to reload the page
-            })
-            .catch((err) => {
-                console.error('An error occurred:', err);
-            });
-        };
-      };
-    //---------------------------------------------------------------------------------------------
-
-    return(
-        <div className='reviewCard'>        
-                <div className='reviewCardHeading'>
-                    <h2>A review by {props.userId}</h2>
-
-                    {/* the update and delete button are hidden unless props.Id = userName */}                    
-                    {
-                        props.userId === sessionStorage.getItem('userName') && (
-                        <>
-                            <button onClick={handleUpdateClick}>Update</button>
-                            <button onClick={handleDeleteClick}>Delete</button>
-                        </>
-                        )
-                    }
+  const handleUpvoteClick = () => {
+    if (!isUpvoted) {
+      // If not upvoted, add the upvote
+      fetch(`${process.env.REACT_APP_DOMAIN}/review/upvote/${props._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userId: sessionStorage.getItem('userName') })
+      })
+        .then(response => response.json())
+        .then(data => {
+          // console.log(data);
+          setIsUpvoted(true);
+          setCurrentUpvotes(currentUpvotes + 1);
+          if (isDownvoted) {
+            setIsDownvoted(false);
+            setCurrentDownvotes(currentDownvotes - 1);
+          }
+        })
+        .catch(error => {
+          console.error('Error upvoting review:', error);
+        });
+    } else {
+      // If already upvoted, remove the upvote
+      fetch(`${process.env.REACT_APP_DOMAIN}/review/upvote/${props._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userId: sessionStorage.getItem('userName') })
+      })
+        .then(response => response.json())
+        .then(data => {
+          setIsUpvoted(false);
+          setCurrentUpvotes(currentUpvotes - 1); // Decrease the upvote count
+        })
+        .catch(error => {
+          console.error('Error removing upvote:', error);
+        });
+    };
+  };
 
 
-                    <h4>Date: {createdDate}</h4>
-                    {/* <h4>Rating: {stars} {props.rating}</h4> */}
-                    <h4>Rating: <Rating name="read-only" value={ Math.round(props.rating)/2} precision={0.5} readOnly size="small" /> {props.rating}</h4>
+  const handleDownvoteClick = () => {
+    if (!isDownvoted) {
+      console.log('test');
+      // If not downvoted, add the downvote
+      fetch(`${process.env.REACT_APP_DOMAIN}/review/downvote/${props._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userId: sessionStorage.getItem('userName') })
+      })
+        .then(response => response.json())
+        .then(data => {
+          setIsDownvoted(true);
+          setCurrentDownvotes(currentDownvotes + 1);
+          if (isUpvoted) {
+            setIsUpvoted(false); // Remove the upvote if it exists
+            setCurrentUpvotes(currentUpvotes - 1);
+          }
+        })
+        .catch(error => {
+          console.error('Error downvoting review:', error);
+        });
+    } else {
+      // If already downvoted, remove the downvote
+      fetch(`${process.env.REACT_APP_DOMAIN}/review/downvote/${props._id}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ userId: sessionStorage.getItem('userName') })
+      })
+        .then(response => response.json())
+        .then(data => {
+          setIsDownvoted(false);
+          setCurrentDownvotes(currentDownvotes - 1);
+        })
+        .catch(error => {
+          console.error('Error removing downvote:', error);
+        });
+    }
+  };
 
-                    <div className='reviewCardConent'>
-                        <p>
-                            {props.reviewBody}                        
-                        </p>
 
-                        {/* for update review, this part is hidden unless the user click the update button */}
-                        {isUpdating && (            
-                            <>
-                                <Box className='rating_submit' sx={{
-                                        width: 200,
-                                        display: 'flex',
-                                        alignItems: 'center',
-                                    }}
-                                    >
-                                    <Rating name="update-rating" 
-                                        value={updatedRating} 
-                                        precision={0.5}
-                                        onChange={(event, newValue) => setUpdatedRating(newValue)} size="small" 
-                                        onChangeActive={(event, newHover) => {
-                                            setHover(newHover);
-                                        }}
-                                    />
-                                    {updatedRating !== null && (
-                                            <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : updatedRating]}</Box>
-                                        )}
+  //---------------------------------------------------------------------------------------------
 
-                                </Box>
-                                <textarea cols='50' rows='8' value={updatedReviewBody} onChange={e => setUpdatedReviewBody(e.target.value)} />
-                                <br></br>
-                                
-                                <button onClick={handleUpdateSubmit}>Submit</button>{isSubmitted && <strong>Review submitted</strong>}
-                            </>
-                        )}
-                    </div>                    
-                </div>
-        </div>
-    );
+  return (
+    <div className='reviewCard'>
+      <div className='reviewCardHeading'>
+        <h2>A review by {props.userId}</h2>
+
+        {/* the update and delete button are hidden unless props.Id = userName */}
+        {
+          props.userId === sessionStorage.getItem('userName') && (
+            <>
+              <button onClick={handleUpdateClick}>Update</button>
+              <button onClick={handleDeleteClick}>Delete</button>
+            </>
+          )
+        }
+
+      </div>
+
+      <h4>Date: {createdDate}</h4>
+      {/* <h4>Rating: {stars} {props.rating}</h4> */}
+      <h4>Rating: <Rating name="read-only" value={Math.round(props.rating) / 2} precision={0.5} readOnly size="small" /> {props.rating}</h4>
+
+      <div className='reviewCardContent'>
+        <p>
+          {props.reviewBody}
+        </p>
+
+        {/* for update review, this part is hidden unless the user click the update button */}
+        {isUpdating && (
+          <>
+            <Box className='rating_submit' sx={{
+              width: 200,
+              display: 'flex',
+              alignItems: 'center',
+            }}
+            >
+              <Rating name="update-rating"
+                value={updatedRating}
+                precision={0.5}
+                onChange={(event, newValue) => setUpdatedRating(newValue)} size="small"
+                onChangeActive={(event, newHover) => {
+                  setHover(newHover);
+                }}
+              />
+              {updatedRating !== null && (
+                <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : updatedRating]}</Box>
+              )}
+
+            </Box>
+            <textarea cols='50' rows='8' value={updatedReviewBody} onChange={e => setUpdatedReviewBody(e.target.value)} />
+            <br></br>
+
+            <button onClick={handleUpdateSubmit}>Submit</button>{isSubmitted && <strong>Review submitted</strong>}
+          </>
+        )}
+      </div>
+      <div className='voteContainer'>
+        <button className={`voteButtons ${isUpvoted ? 'upvoted' : ''}`} onClick={handleUpvoteClick}>▲  {currentUpvotes}</button>
+        <button className={`voteButtons ${isDownvoted ? 'downvoted' : ''}`} onClick={handleDownvoteClick}>▼  {currentDownvotes}</button>
+      </div>
+    </div>
+  );
 };
 
 export default ReviewCard;
-
-
-/*
-const reviewBody = isExpanded ? props.reviewBody : props.reviewBody.substring(0,500);        //only show the first 500 characters
-
-<div className='reviewCardConent'>
-                        <p>
-                        {reviewBody}
-                        {!isExpanded && (
-                            <a href="#" onClick={(event) => {
-                                event.preventDefault(); // Prevent moving to the top of the page
-                                setIsExpanded(true);
-                            }}
-                            >
-                            ...read more...
-                            </a>
-                        )}
-                        </p>
-                    </div>
-*/
-
-    /*
-    const stars = Array.from({ length: Math.round(props.rating)/2 }, 
-    (_, index) => (
-        <FaStar key={index} className='starStyle' />        //calculate no.of star based on vote_average value
-      ));
-    */
-    //console.log("props:");
-    //console.log(props);
--- src/components/ReviewCardList.js
+++ src/components/ReviewCardList.js
@@ -2,35 +2,37 @@ import React from 'react';
 import ReviewCard from './ReviewCard';
 import '../css/reviewCard.css';
 
-const ReviewCardList = ({reviewData}) => {
+const ReviewCardList = ({ reviewData }) => {
     //console.log("In ReviewCardList:")
     //console.log(reviewData);
     if (Array.isArray(reviewData) && reviewData.length > 0) {
         return (
-            <div className='reviewCardContainer'>            
-                    {
-                        //reviewData.results.map((review, i) => {       //for TMDB API
-                        reviewData.map((review, i) => {
-                            return(
-                                <ReviewCard
-                                    key = {review._id}
-                                    _id = {review._id}
-                                    userId = {review.userId}
-                                    createdDate = {review.createdDate}                                
-                                    rating = {review.rating}
-                                    reviewBody = {review.reviewBody}
-                                />
-                            );
-                        })
-                    }            
+            <div className='reviewCardContainer'>
+                {
+                    //reviewData.results.map((review, i) => {       //for TMDB API
+                    reviewData.map((review, i) => {
+                        return (
+                            <ReviewCard
+                                key={review._id}
+                                _id={review._id}
+                                userId={review.userId}
+                                createdDate={review.createdDate}
+                                rating={review.rating}
+                                reviewBody={review.reviewBody}
+                                upvotes={review.upvotes}
+                                downvotes={review.downvotes}
+                            />
+                        );
+                    })
+                }
             </div>
-        )
+        );
     } else {
         return <div className='reviewCardContainer'>
-                    <div className='noReview'>
-                        <h2>No review for this movie.</h2>
-                    </div>
-                </div>;
+            <div className='noReview'>
+                <h2>No review for this movie.</h2>
+            </div>
+        </div>;
     }
 };
 
